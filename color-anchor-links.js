57e7c2314ad7fc3173f0898084ed97931a7535c1
Clepter <84567842+ClepterLT@users.noreply.github.com>
2021-08-05 08:42:00 +0300

    Create color-anchor-links.js

---
+++ color-anchor-links.js
@@ -0,0 +1,27 @@
+function colorActiveAnchorLink () {
+  const anchorLinks = document.querySelectorAll(".js-anchor-link");
+  const textParagraphs = document.querySelectorAll(".js-anchored-text-paragraph");
+  
+  const options = {
+        root: null,
+        threshold: 1,
+  }
+  
+  const handleIntersection = function(entries) {
+        entries.forEach(function(entry, index) {
+            if(entry.isIntersecting) {
+                const intersectingParagraph = entry.target;
+                const intersectingParagraphId = `#${intersectingParagraph.getAttribute('id')}`;
+                anchorLinks.forEach(function(link) {
+                    const linkHref = link.getAttribute('href');
+                    link.classList.remove("anchor-link--active");
+                    if(intersectingParagraphId === linkHref) {
+                        link.classList.add("anchor-link--active");
+                    }
+                });
+            }
+        });
+    }
+  
+  const linkObserver = new IntersectionObserver(handleIntersection, options);
+}
